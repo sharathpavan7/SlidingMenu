cbb06eed1a3c43c6eb30b6f47c37b77b752b8c16
YakseeHyd <suresh@yaksee.com>
2017-12-06 15:17:03 +0530

    New Activity class fiels & layout files are added to the project .

---
+++ app/src/main/java/com/sharath/slidingmenu/activity/PrivacyPolicy.java
@@ -0,0 +1,15 @@
+package com.sharath.slidingmenu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sharath.slidingmenu.R;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
